85b9130739d3e20138431a1e609f9afe432ceb5c
Haeseok Maeng <Haeseok.Maeng@gmail.com>
2020-12-25 20:53:05 +0900

    modified for static files and add base.css

--- pragamatic/pragamatic/settings.py
+++ pragamatic/pragamatic/settings.py
@@ -21,7 +21,6 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 # reading .env file
-print('BASE_DIR : ', BASE_DIR)
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
@@ -129,3 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
