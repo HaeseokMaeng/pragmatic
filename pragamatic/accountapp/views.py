21eb4330622c6dfdb0d624daad10c13d88dc0a4f
Haeseok Maeng <Haeseok.Maeng@gmail.com>
2020-12-26 14:04:53 +0900

    1st mod for POST

--- pragamatic/accountapp/views.py
+++ pragamatic/accountapp/views.py
@@ -2,6 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .models import HelloWorld
+
 
 def Hello(request) :
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST" :
+        temp = request.POST.get('hello_world_input')
+
+        tmp_HelloWorld = HelloWorld()
+        tmp_HelloWorld.text = temp
+        tmp_HelloWorld.save()
+
+        return render(request, 'accountapp/hello_world.html', context={"hello_world" : tmp_HelloWorld, })
+    else :
+        return render(request, 'accountapp/hello_world.html', context={"text": 'GET METHOD !!!', })
