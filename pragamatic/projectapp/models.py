64b3d78f9f26f6c657c72cb75c7e67cecc4b5ad4
Haeseok Maeng <Haeseok.Maeng@gmail.com>
2020-12-29 22:27:58 +0900

    Implement projectApp

---
+++ pragamatic/projectapp/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Project(models.Model) :
+    title = models.CharField(max_length=20, null=True)
+    description = models.CharField(max_length=250, null=True)
+    img = models.ImageField(upload_to='project/', null=False)
+    create_at = models.DateField(auto_created=True, null=True)
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='project', null=True)
